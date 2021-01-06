a4823b433cfefc9eb7678ae2772fced4a371679b
Nafies Luthfi <nafiesl@gmail.com>
2021-01-06 21:50:45 +0800

    Use global container div for the views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,7 @@
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
                             @endif
-                            
+
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -76,7 +76,7 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div class="container">@yield('content')</div>
         </main>
     </div>
 </body>
